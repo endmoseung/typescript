52a111751a9f996701fdbbd85bd4435fa8508bdf
moseung <tmdah900@naver.com>
2022-08-02 12:49:03 +0900

    first enroll

---
+++ 1-types/1-1-basic.ts
@@ -0,0 +1,35 @@
+{
+  //number 
+  const num:number =-2;
+
+  //string
+  const str: string = "hihi"
+  console.log(str);
+
+  //boolean 
+  const boal:boolean = true;
+
+  //undefined 값이 정해진지 안정해진지 모르는 상황
+  let name:undefined; //💩
+  let names:number|undefined; //보편적으로 null보다는 undefined을 씀
+
+  //null 값이 아에 정해지지 않은상황
+  let abc:null ;//💩
+  let abc2:string|null;
+
+  //unknown 어떤 type이 들어올지 모르는것  //💩
+  let notSure:unknown = 2;
+  notSure="adaf";
+
+  //any   //💩
+  let anything:any = 2;
+  anything = "bagga";
+
+  // void 함수에서 어떤값을 리턴하지 않을떄 사용 써도되고 안써도됨
+  function print():void {
+    console.log("hello");
+    return ;
+  }
+  
+  // never   
+}
---
+++ 1-types/1-2-function.ts
@@ -0,0 +1,53 @@
+{
+  //   //javascript
+  // function jsADd(num1,num2){
+  //   return num1+num2;
+  // }
+
+
+  // //typescript
+  // function tsAdd(num1:number,num2:number):number{
+  //   return num1+num2;
+  // }
+
+  // //javascript
+  // function jsFetch(id){
+  //   return new Promise((resolve,reject)=>{
+  //     resolve(100)
+  //   })
+  // }
+
+  // //typescript
+  // function tsFetch(id:string):Promise<number>{
+  //   return new Promise((resolve,reject)=>{
+  //     resolve(100)
+  //   })
+  // }
+  //javascript => typescript
+  //optional parameter ?
+  function printName (firstname:string,lastname?:string){ // :앞에?을 붙이면 이 함수는 전달받을수도 있고 안받을수도 있다는걸 표현
+    console.log(firstname);
+    console.log(lastname);
+  }
+  printName("steve","jobs");
+  printName("seung mo");
+
+  //Default Parameter 값을 전달받는게 없으면 default메시지를 출력한다.
+  function printMessage(message:string="default"){
+console.log(message)
+  }
+  printMessage("hihi")
+  printMessage();
+
+  //Rest Parameter
+  function addNumbers(...numbers:number[]):number{
+    let total:number = 0
+    for(let x of numbers){
+      total+=x;
+    }
+    return total;
+  }
+  console.log(addNumbers(1,2,3,4))
+  console.log(addNumbers(1,2,3,4,5))
+  console.log(addNumbers(1,2,3,4,5,6))
+}
