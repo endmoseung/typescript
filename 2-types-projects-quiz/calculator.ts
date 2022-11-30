4b45e9ca09ed29b450557b9d78d880c32b643ac9
moseung <tmdah900@naver.com>
2022-11-30 21:59:43 +0900

    ✨ 11/30 studied

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -3,17 +3,41 @@
  */
 
 type Tool = "add" | "subtract" | "multiply" | "divide" | "remainder"; // 가독성을 위해서 함수내 인자에 너무 길다면 따로 type을 선언해준다.
+
+const addValues = (a: number, b: number): number => {
+  return a + b;
+};
+
+const divideValues = (a: number, b: number): number => {
+  return a / b;
+};
+
+const multiplyValues = (a: number, b: number): number => {
+  return a * b;
+};
+
+const subtractValues = (a: number, b: number): number => {
+  return a - b;
+};
+
+const remainderValues = (a: number, b: number): number => {
+  return a % b;
+};
+
 const calculate = (tool: Tool, a: number, b: number): number => {
-  if (tool === "add") {
-    return a + b;
-  } else if (tool === "subtract") {
-    return a - b;
-  } else if (tool === "multiply") {
-    return a * b;
-  } else if (tool === "divide") {
-    return a / b;
-  } else {
-    return a % b;
+  switch (tool) {
+    case "add":
+      return addValues(a, b);
+    case "divide":
+      return divideValues(a, b);
+    case "subtract":
+      return subtractValues(a, b);
+    case "multiply":
+      return multiplyValues(a, b);
+    case "remainder":
+      return remainderValues(a, b);
+    default:
+      throw new Error("unknown Action");
   }
 };
 console.log(calculate("add", 1, 3)); // 4
--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,36 +1,34 @@
-/**
- * Let's make a game 🕹
- */
-type Action = "up" | "down" | "left" | "right";
+{
+  /**
+   * Let's make a game 🕹
+   */
+  type Action = "up" | "down" | "left" | "right";
 
-let position = { x: 0, y: 0 };
-const move = (action: Action): number => {
-  let x: number = 0;
-  let y: number = 0;
-  switch (action) {
-    case "up":
-      return position.y++;
-      break;
-    case "down":
-      return position.y--;
-      break;
-    case "left":
-      return position.x--;
-      break;
-    case "right":
-      return position.x++;
-      break;
-    default:
-      throw Error("unknown action");
-  }
-};
+  let position = { x: 0, y: 0 };
+  const move = (action: Action): number => {
+    let x: number = 0;
+    let y: number = 0;
+    switch (action) {
+      case "up":
+        return position.y++;
+      case "down":
+        return position.y--;
+      case "left":
+        return position.x--;
+      case "right":
+        return position.x++;
+      default:
+        throw Error("unknown action");
+    }
+  };
 
-console.log(position); // { x: 0, y: 0}
-move("up");
-console.log(position); // { x: 0, y: 1}
-move("down");
-console.log(position); // { x: 0, y: 0}
-move("left");
-console.log(position); // { x: -1, y: 0}
-move("right");
-console.log(position); // { x: 0, y: 0}
+  console.log(position); // { x: 0, y: 0}
+  move("up");
+  console.log(position); // { x: 0, y: 1}
+  move("down");
+  console.log(position); // { x: 0, y: 0}
+  move("left");
+  console.log(position); // { x: -1, y: 0}
+  move("right");
+  console.log(position); // { x: 0, y: 0}
+}
